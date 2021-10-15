e072d5dfe0de19186d2604a0733f87275fbb020c
Arunima Mittra <arunima1820@gmail.com>
2021-10-15 10:57:58 -0500

    Filtereing works!!

--- front-end/src/pages/TagTester.js
+++ front-end/src/pages/TagTester.js
@@ -7,15 +7,27 @@ import Col from 'react-bootstrap/esm/Col';
 import Autocomplete from '@mui/material/Autocomplete';
 import TextField from '@mui/material/TextField';
 import { apiProvider } from '../providers/Provider';
+import { AllOrgContext } from '../providers/AllOrgProvider';
 
 export default function TagTester() {
     const [tags, setTags] = useState([])
+    const orgs = useContext(AllOrgContext)
 
     useEffect(() => {
         apiProvider.getAll('tags', setTags)
     }, [])
 
     const sorted = tags.sort((a, b) => a.category.toString() < b.category.toString() ? 1 : -1)
+
+    function handleChange(tags) {
+        console.log("VALUE", tags)
+        var filteredOrgs = []
+        for (const tag of tags) {
+            if (tag.category == 'Organization') 
+                filteredOrgs.push(orgs.find(org => org.uId === tag.id))
+        }
+        console.log(filteredOrgs)    
+    }
 
     return (
         <div>
@@ -27,7 +39,7 @@ export default function TagTester() {
                 options={sorted}
                 groupBy={(option) => option.category.toString()}
                 getOptionLabel={(option) => (option.acronym.toString() || option.value.toString())}
-                //onChange={(e, value, _) => setOrgFilterValue(getOrgIds(value))}
+                onChange={(e, value, _) => handleChange(value)}
                 loading={tags.length === 0}
                 renderInput={(params) => 
                     <TextField 
